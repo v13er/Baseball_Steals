dac51b31e0fcde104dee82c7e8911ca9cd85ef42
Adam <pastawater74@gmail.com>
2022-09-24 17:29:56 +0300

    Added basic input for signs
    This will in the future hopefully be changed for a gui based application like the mark rober video but, for now, this works.

--- steals.py
+++ steals.py
@@ -1,9 +1,19 @@
 #Constants 
-def signs():
+def input_signs():
+    #Makes an empty list of signs 
+    signs = []
     signals = True
+    while signals:
+        prompt = input("Please input a sign (Write exit to stop)\n> ")
+        prompt = prompt.lower()
+        if prompt == "exit":
+            break
+
+    return signs
+
 
 def main():
-    signs()
+    signs = input_signs()
 
 if __name__ == '__main__':
     main()
