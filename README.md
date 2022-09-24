8c6935658c7a935bd996f74f9f00cfe62f805ea5
Adam <pastawater74@gmail.com>
2022-09-24 17:46:08 +0300

    Added stuff 1. Except for KeyboardInterupt 2. Doesnt add empty sign

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # baseball_steals
 Based on a mark rober video about baseball steals. Wanted to try it out.
+
+(Sorry if I commit a lot lol, my first real github repo and wanna use git a lot)
+
+TODO:
+Assign a letter to each sign and allow user to display them if forgotten
--- steals.py
+++ steals.py
@@ -1,14 +1,19 @@
-#Constants 
 def input_signs():
     #Makes an empty list of signs 
     signs = []
+
     signals = True
     while signals:
         prompt = input("Please input a sign (Write exit to stop)\n> ")
         prompt = prompt.lower()
+        #Exits if condition is met
         if prompt == "exit":
             break
-        signs.append(prompt)
+        elif prompt == "":
+            continue
+        #Appends to signs as more are added
+        else:
+            signs.append(prompt)
 
     return signs
 
@@ -16,6 +21,10 @@ def input_signs():
 def main():
     signs = input_signs()
     print(signs)
+    
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nHave a nice day!")
