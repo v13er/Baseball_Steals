0aa59530cabec4a3ce0f268548bafb5a20503205
Adam <pastawater74@gmail.com>
2022-09-24 20:51:35 +0300

    Added the ability to write sequences, they are all stored to a list

--- steals.py
+++ steals.py
@@ -2,31 +2,50 @@ def input_signs():
     #Makes an empty list of signs 
     signs = []
 
+    #Gets all the signals
     signals = True
     while signals:
         prompt = input("Please input a sign (Write exit to stop)\n> ")
         prompt = prompt.lower()
-        #Exits if condition is met
-        if prompt == "exit":
-            break
-        elif prompt == "":
-            continue
-        #Appends to signs as more are added
-        else:
-            signs.append(prompt)
-
+        match prompt:
+            case "exit":
+                break
+            case "":
+                continue
+            case _:
+                signs.append(prompt)
     return signs
 
 def process_signs(signs):
+    #Assigns each motion a symbol
     sign_symbols = {}
     for sign in range(0, len(signs)):
         sign_symbols[chr(sign+65)] = signs[sign]        
     return sign_symbols
 
+def steal_sequences(sign_symbols):
+    #Gets all the sequences
+    sequence = True
+    sequences = []
+    while sequence:
+        prompt = input("Please input a sequence of symbols (Write done when finished)\n> ")
+        sqnc = tuple(prompt.lower())
+        match prompt:
+            case "done":
+                break
+            case "":
+                continue
+            case _:
+                sequences.append(sqnc)
+
+    return sequences
+
 def main():
     signs = input_signs()
-    process_signs(signs)
-
+    sign_symbols = process_signs(signs)
+    print(f'You sign symbols are {sign_symbols} (Remember these)!')
+    sequences = steal_sequences(sign_symbols)
+    print(sequences)
 
 if __name__ == '__main__':
     try:
